7420a1a45253df7d0ab5987c8d3d57b9bccc99f1
Hos9 <shahin9315@gmail.com>
2026-03-06 23:04:40 +0600

    JS applied

---
+++ scripts/login.js
@@ -0,0 +1,17 @@
+document.getElementById("login-btn").addEventListener("click", () => {
+  const userName = document.getElementById("input-username").value;
+  console.log(userName);
+
+  const password = document.getElementById("input-pw").value;
+  console.log(password);
+
+  if (userName == "admin" && password == "admin123") {
+    alert("Login Success");
+    window.location.assign("./all.html");
+  } else {
+    alert(`
+    Login Failed!!
+    Username or Password not matched!!
+    `);
+  }
+});
